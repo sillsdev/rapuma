d7d3e78afa3c768706228a8c1d32d9ce0cc2334b
dennis_drescher <dennis_drescher@sil.org>
2012-11-19 14:00:14 +0700

    Tweaks to the demo, ect.

--- rpm_dev.sh
+++ rpm_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gnome-terminal --geometry=80x20 --tab --working-directory="Publishing/testArea/rpmScripts/KYU-MYMR-MRK" &
+gnome-terminal --geometry=80x20 --tab --working-directory="Publishing/testArea" &
 gedit &
-nautilus ~/Publishing/testArea/rpmScripts/KYU-MYMR-MRK &
+nautilus ~/Publishing/testArea/KYU-MYMR-MRK &
 #libreoffice --calc ~/Projects/rpm/todo.ods
