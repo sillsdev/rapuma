759442ec9e52f1df981f1fe7ca38299b25336a85
dennis_drescher <dennis_drescher@sil.org>
2013-02-05 14:51:46 +0700

    Worked on fixing KYUM example (more work needed)

--- examp_jas.sh
+++ examp_jas.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+rapuma project ENG-LATN-JAS -r
 
+rapuma project ENG-LATN-JAS -m book -n "The Book of James in English" -g ~/Publishing/testArea
+
+rapuma component ENG-LATN-JAS usfm james -a -i jas -s ~/Publishing/testArea/MBJAS
+
+rapuma component ENG-LATN-JAS usfm james -e
