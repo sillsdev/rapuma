2544b460f6ba09a899227996ccd646f733ae2851
dennis_drescher <dennis_drescher@sil.org>
2013-04-23 21:24:09 +0700

    Update general test script

--- mrk-example.sh
+++ mrk-example.sh
@@ -9,7 +9,7 @@ rapuma project KYU-MYMR-MRKA -d
 # Create the project
 rapuma project KYU-MYMR-MRKA -t ~/Publishing/testArea -m book -n 'Kayah Book of Mark example in the Burmese script'
 # Install a group to render
-rapuma group KYU-MYMR-MRKA Mark -c usfm -a -i mrk -d MYMR -s ~/Publishing/testArea/KYUM/pt_environ
+rapuma group KYU-MYMR-MRKA Mark -c usfm -m add -i mrk -d MYMR -s ~/Publishing/testArea/KYUM/pt_environ
 # Set the font to the right one
 rapuma font KYU-MYMR-MRKA Mark usfm Padauk -m -f
 # Turn on font features in Padauk & adjust default settings
@@ -32,12 +32,12 @@ cp ~/Publishing/testArea/KYUM/resources/Mark_groupPreprocess.py ~/Publishing/tes
 echo copying: usfm-ext.sty
 cp ~/Publishing/testArea/KYUM/resources/usfm-ext.sty ~/Publishing/testArea/KYU-MYMR-MRKA/Styles/usfm-ext.sty
 # Now update the text so the prprocess is used
-rapuma group KYU-MYMR-MRKA Mark -u -f
+rapuma group KYU-MYMR-MRKA Mark -m update -f
 # Install and switch font (Padauk 2.95 has a bug)
 rapuma font KYU-MYMR-MRKA Mark usfm -a Padauk_2.701
 rapuma font KYU-MYMR-MRKA Mark usfm -m -f Padauk_2.701
 # Render the component
-rapuma group KYU-MYMR-MRKA Mark -e
+rapuma group KYU-MYMR-MRKA Mark -m execute
 # Backup the project
 rapuma project KYU-MYMR-MRKA -p backup
 
