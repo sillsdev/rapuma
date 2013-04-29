4bf2ff2a1b6017649a84a645b17bc58d8a06a87b
dennis_drescher <dennis_drescher@sil.org>
2013-04-29 09:44:39 +0700

    Adding binding module in project area - Check point, do not use!

---
+++ lib/rapuma/manager/binding.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf_8 -*-
+
+# By Dennis Drescher (dennis_drescher at sil.org)
+
+###############################################################################
+######################### Description/Documentation ###########################
+###############################################################################
+
+# This class will handle book project tasks.
+
+
+###############################################################################
+################################# Project Class ###############################
+###############################################################################
+# Firstly, import all the standard Python modules we need for
+# this process
+
+import os, shutil
+
+# Load the local classes
+from rapuma.core.tools          import Tools
+from rapuma.project.manager     import Manager
+
+
+###############################################################################
+################################## Begin Class ################################
+###############################################################################
+
+class Binding (Manager) :
+
+    def __init__(self, project, cfg, cType) :
+        '''Do the primary initialization for this manager.'''
+
+        super(Layout, self).__init__(project, cfg)
+
+        # List the renderers this manager supports
+        self.tools                          = Tools()
+        self.cType                          = cType
+        self.Ctype                          = cType.capitalize()
+        self.manager                        = 'pdf_Binding'
+        self.project                        = project
+        self.managers                       = project.managers
+        self.projConfig                     = self.project.projConfig
+        self.projMediaType                  = self.project.projectMediaIDCode
+        self.rapumaXmlBindConfig            = os.path.join(self.local.rapumaConfigFolder, 'binding.xml')
+        # File names
+
+        # Paths
+
+        # Files with paths
+        # Set local var and override in project object (if needed)
+        self.project.local.layoutConfFile   = self.layoutConfFile
+
+        # Get persistant values from the config if there are any
+        newSectionSettings = self.tools.getPersistantSettings(self.project.projConfig['Managers'][self.manager], self.rapumaXmlBindConfig)
+        if newSectionSettings != self.project.projConfig['Managers'][self.manager] :
+            self.project.projConfig['Managers'][self.manager] = newSectionSettings
+            self.tools.writeConfFile(self.project.projConfig)
+
+        self.compSettings = self.project.projConfig['Managers'][self.manager]
+
+        for k, v in self.compSettings.iteritems() :
+            setattr(self, k, v)
+
+
+
+        # Log messages for this module
+        self.errorCodes     = {
+
+            '0000' : ['MSG', 'Placeholder message'],
+
+        }
+
+###############################################################################
+############################ Manager Level Functions ##########################
+###############################################################################
+
+
+# FIXME: We may want to move to this manager some more general functions
+# from other managers
+
+
