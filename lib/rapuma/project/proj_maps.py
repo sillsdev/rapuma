87bd4f8a315a5c3620882fb67a339b7426155d0a
dennis_drescher <dennis_drescher@sil.org>
2013-05-14 11:23:54 +0700

    Working on map module - test

--- lib/rapuma/project/proj_maps.py
+++ lib/rapuma/project/proj_maps.py
@@ -51,17 +51,18 @@ class Maps (object) :
         self.defaultXmlConfFileName     = 'proj_maps.xml'
         self.mapsConfFileName           = 'maps.conf'
         # Paths
+        self.projConfFolder             = self.local.projConfFolder
         self.projMapsFolder             = os.path.join(self.local.projIllustrationsFolder, 'Maps')
         self.rpmRapumaConfigFolder      = self.local.rapumaConfigFolder
         # File names with paths
-        self.projMapXmlFile             = os.path.join(self.rpmRapumaConfigFolder, self.defaultXmlConfFileName)
         self.mapsConfFile               = os.path.join(self.projConfFolder, self.mapsConfFileName)
+        self.defaultXmlConfFile         = os.path.join(self.rpmRapumaConfigFolder, self.defaultXmlConfFileName)
 
         # Load the config object
         self.mapsConfig                 = self.tools.initConfig(self.mapsConfFile, self.defaultXmlConfFile)
-        # Load settings from the manager config
-        for k, v in self.projConfig['Managers'][self.manager].iteritems() :
-            setattr(self, k, v)
+#        # Load settings from the manager config
+#        for k, v in self.mapsConfig['Maps'][self.manager].iteritems() :
+#            setattr(self, k, v)
 
 
         # Log messages for this module
@@ -70,7 +71,7 @@ class Maps (object) :
             '0205' : ['MSG', 'Unassigned message.'],
             '0210' : ['LOG', 'Wrote out map settings to the project configuration file.'],
             '0215' : ['ERR', 'Failed to write out project map settings to the project configuration file.'],
-            '0220' : ['LOG', 'Added maps to project.'],
+            '0220' : ['MSG', 'Added maps to project.'],
             '0230' : ['MSG', 'Maps have been removed from the project configuration.'],
             '0240' : ['MSG', 'Rendered the project maps.'],
             '0250' : ['LOG', 'Created project maps folder.'],
@@ -89,10 +90,10 @@ class Maps (object) :
     def addMaps (self, mapList) :
         '''Add maps to the project.'''
 
-        self.createMapSettings()
-        self.projConfig['Groups']['Maps']['mapFileList'] = mapList
-        if self.tools.writeConfFile(self.projConfig) :
-            self.log.writeToLog(self.errorCodes['0220'])
+        self.createMapsFolder()
+        self.mapsConfig['ProjMapInfo']['mapFileList'] = mapList
+        self.tools.writeConfFile(self.mapsConfig)
+        self.log.writeToLog(self.errorCodes['0220'])
 
 
     def removeMaps (self) :
@@ -115,31 +116,6 @@ class Maps (object) :
             self.log.writeToLog(self.errorCodes['0250'])
 
 
-    def createMapSettings (self) :
-        '''Create a map group and settings in the project config file.'''
-
-        # At this point we are under the assumption that only one map group is needed
-        if not self.tools.isConfSection(self.projConfig['Groups'], 'Maps') :
-            self.tools.buildConfSection(self.projConfig['Groups'], 'Maps')
-
-        # Update settings if needed
-        update = False
-        mapDefaults = self.tools.getXMLSettings(self.projMapXmlFile)
-        for k, v, in maps.iteritems() :
-            # Do not overwrite if a value is already there
-            if not self.tools.testForSetting(self.projConfig['Groups']['Maps'], k) :
-                self.projConfig['Groups']['Maps'][k] = v
-                # If we are dealing with an empty string, don't bother writing out
-                # Trying to avoid needless conf updating here. Just in case we are
-                # working with a list, we'll use len()
-                if len(v) > 0 :
-                    update = True
-        # Update the conf if one or more settings were changed
-        if update :
-            if self.tools.writeConfFile(self.projConfig) :
-                self.log.writeToLog(self.errorCodes['0210'])
-            else :
-                self.log.writeToLog(self.errorCodes['0215'])
 
 
 
