ee6a3e45e8f36f2e5a580ce2954be3ff09c03c6d
dennis_drescher <dennis_drescher@sil.org>
2014-01-20 16:41:04 +0700

    missed file

---
+++ lib/rapuma/dialog/new_ctrl.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#    Copyright 2014, SIL International
+#    All rights reserved.
+#
+#    This library is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation; either version 2.1 of License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should also have received a copy of the GNU Lesser General Public
+#    License along with this library in the file named "LICENSE".
+#    If not, write to the Free Software Foundation, 51 Franklin Street,
+#    suite 500, Boston, MA 02110-1335, USA or visit their web page on the 
+#    internet at http://www.fsf.org/licenses/lgpl.html.
+
+
+import os, sys
+
+# Load GUI modules
+from PySide                             import QtGui, QtCore
+from PySide.QtGui                       import QDialog, QApplication, QMessageBox
+from PySide.QtCore                      import QPropertyAnimation
+from rapuma.dialog                      import new_dlg
+
+class NewCtrl (QDialog, QPropertyAnimation, new_dlg.Ui_NewProject) :
+
+    def __init__ (self, parent=None) :
+        '''Initialize and start up the UI'''
+
+        super(NewCtrl, self).__init__(parent)
+
+        #self.setWindowIcon(appicon)
+        self.setupUi(self)
+
+
+    def main (self) :
+        '''This function shows the main dialog'''
+
+        self.show()
+
+
+###############################################################################
+################################ Menu Items ###################################
+###############################################################################
+
+
+
+###############################################################################
+############################## Dialog Starts Here #############################
+###############################################################################
+
+if __name__ == '__main__' :
+
+    app = QApplication(sys.argv)
+    window = NewCtrl()
+    window.main()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+
+
