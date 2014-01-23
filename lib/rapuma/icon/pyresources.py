0c440fbe72fea0228b017b54b9377bf6513ea0ce
dennis_drescher <dennis_drescher@sil.org>
2014-01-23 08:46:28 +0700

    Reorg/relable menu - testing

--- lib/rapuma/icon/pyresources.py
+++ lib/rapuma/icon/pyresources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Thu Jan 23 04:59:10 2014
+# Created: Thu Jan 23 08:37:33 2014
 #      by: The Resource Compiler for PySide (Qt v4.8.0)
 #
 # WARNING! All changes made in this file will be lost!
