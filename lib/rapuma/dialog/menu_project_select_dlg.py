67b7258607aee32e2e9e5c5c0ceea123ee651f48
dennis_drescher <dennis_drescher@sil.org>
2014-02-05 11:15:01 +0700

    fix usfmTex long v line issue - testing

--- lib/rapuma/dialog/menu_project_select_dlg.py
+++ lib/rapuma/dialog/menu_project_select_dlg.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/dennis/Projects/rapuma/lib/rapuma/dialog/menu_project_select_dlg.ui'
 #
-# Created: Wed Feb  5 08:30:28 2014
+# Created: Wed Feb  5 11:13:04 2014
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
