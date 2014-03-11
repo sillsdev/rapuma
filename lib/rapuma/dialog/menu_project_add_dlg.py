00984a7fbf52aef14f064c84c8b67568618e862c
dennis_drescher <dennis_drescher@sil.org>
2014-03-11 13:21:06 +0700

    Working on using icons in dialogs - testing

--- lib/rapuma/dialog/menu_project_add_dlg.py
+++ lib/rapuma/dialog/menu_project_add_dlg.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/nancy/Projects/rapuma/lib/rapuma/dialog/menu_project_add_dlg.ui'
+# Form implementation generated from reading ui file '/home/dennis/Projects/rapuma/lib/rapuma/dialog/menu_project_add_dlg.ui'
 #
-# Created: Mon Mar 10 21:21:05 2014
+# Created: Tue Mar 11 13:19:18 2014
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
