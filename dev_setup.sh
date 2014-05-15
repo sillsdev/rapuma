1f624a9e616535baa4053256c35f4091e2fa52f9
Robin Munn <robin_munn@sil.org>
2014-05-15 10:53:07 +0700

    Fix typo in dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,7 +3,7 @@
 # Setup a development for Rapuma so it runs from the repo
 
 export PYTHONPATH=`pwd`/lib:$PYTHONPATH
-export RAPUMA_CMD=`pwd`/scrips/rapuma
+export RAPUMA_CMD=`pwd`/scripts/rapuma
 export RAPUMA_BASE=`pwd`
 
 export PYTHONPATH=`pwd`/lib:$PYTHONPATH
